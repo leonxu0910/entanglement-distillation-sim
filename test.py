10d0420a962cd5fbe5b8d8e648d7834757cacf6d
Leon Xu <liangxu@stanford.edu>
2019-03-03 14:25:49 -0800

    added bilateral rotations

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import numpy as np
+from math import *
+from cmath import *
 
 # two qubits states in vector representation
 s00 = np.matrix([1,0,0,0])	# |00>
@@ -26,6 +28,14 @@ rho_phi_n = np.outer(phi_n, phi_n)	# |phi-><phi-|
 # unilateral pauli rotations
 uni_rot_y_pi = np.kron(np.matrix([[0,-1],[1,0]]), np.identity(2))	# unilateral pauli-y rotation by pi degrees
 
+# bilateral rotations
+rot_y = np.matrix([[np.sqrt(2)/2,-np.sqrt(2)/2],[np.sqrt(2)/2,np.sqrt(2)/2]])
+bi_rot_y = np.kron(rot_y, rot_y)
+rot_z = np.matrix([[complex(np.sqrt(2)/2, -np.sqrt(2)/2), 0], [0, complex(np.sqrt(2)/2, np.sqrt(2)/2)]])
+bi_rot_z = np.kron(rot_z, rot_z)
+rot_x = np.matrix([[np.sqrt(2)/2, complex(0, -np.sqrt(2)/2)], [complex(0, -np.sqrt(2)/2), np.sqrt(2)/2]])
+bi_rot_x = np.kron(rot_x, rot_x)
+
 # unitary xor operation
 U_xor = np.outer(s11, s10) + np.outer(s10, s11) + np.outer(s00, s00) + np.outer(s01, s01)
 
@@ -37,7 +47,7 @@ def two_qubits_state(prob_psi_n, prob_psi_p, prob_phi_p, prob_phi_n):
 def fidelity(M):
 	# fidelity of a given state M projected onto the singlet state 
 	# F = <psi-|M|psi->
-	return np.inner(psi_n, np.matmul(M, psi_n.T).T)
+	return np.inner(psi_n, np.matmul(M, psi_n.T).T).item(0)
 	
 def purity(M):
 	# purity of the density matrix M
@@ -48,6 +58,11 @@ def purity(M):
 def F_prime(F):
 	# recurrence fidelity of F'
 	return (F**2 + (1/9)*(1-F)**2) / (F**2 + (2/3)*F*(1-F) + (5/9)*(1-F)**2)
+	
+def operate(O, M):
+	# apply operator O onto density matrix M
+	return np.matmul(O, np.matmul(M, O.getH()))
+	
 
 
 # unilateral y rotation test code
@@ -55,10 +70,10 @@ def F_prime(F):
 # Note: R_y(pi) is rotation around y axis by pi degrees which acts only on the first qubit. R_y(pi) = e^(-i*(pi/2)*sigma_y)) where signma_y is pauli-y matrix
 #		I_2 is 2D identity matrix
 #		tp is tensor product
-F = 1	# fidality of Werner state
-mostly_psi_n = two_qubits_state(F, (1-F)/3, (1-F)/3, (1-F)/3)
-mostly_phi_p = two_qubits_state((1-F)/3, (1-F)/3, F, (1-F)/3)
-mostly_psi_n_after = np.matmul(uni_rot_y_pi, np.matmul(mostly_psi_n, uni_rot_y_pi.T))	# after rotation
-print(mostly_psi_n)
-print(mostly_phi_p)
-print(mostly_psi_n_after)	# mostly_psi_n_after should be the same as mostly_phi_p
+# F = 1	# fidelity of Werner state
+# mostly_psi_n = two_qubits_state(F, (1-F)/3, (1-F)/3, (1-F)/3)
+# mostly_phi_p = two_qubits_state((1-F)/3, (1-F)/3, F, (1-F)/3)
+# mostly_psi_n_after = np.matmul(uni_rot_y_pi, np.matmul(mostly_psi_n, uni_rot_y_pi.T))	# after rotation
+# print(mostly_psi_n)
+# print(mostly_phi_p)
+# print(mostly_psi_n_after)	# mostly_psi_n_after should be the same as mostly_phi_p
