cceb5f6961a15e7738c2015a090a058a02ae9a41
srjn94 <srjn94@gmail.com>
2019-03-22 17:43:59 -0700

    g

--- qubit_pair.py
+++ qubit_pair.py
@@ -1,106 +1,35 @@
 import numpy as np
+import random
 
-# initialize seed for reproducibility
 np.random.seed()
+random.seed()
 
-class QubitPair:
-  """A four-vector representation of a qubit pair.
+def _normalize(state):
+  norm = sum(value for value in state.values())
+  for k in state:
+    state[k] /= norm
 
-  By default, it's represented in the Bell basis, but can be configured to be
-  represented in the Binary basis at initialization, or through a class 
-  method. Contains all operations needed for quantum distillation in the form
-  of class methods.
-
-  Operations will be performed in the most appropriate basis, converting back
-  and forth if needed.
-
-  Attributes:
-    vector (np.array): complex state vector, normalized
-    basis (str): either "bell" or "binary"
-
-  """
-
-  # atomic state vectors
-  _ZERO = np.array([1.0, 0.0])
-  _ONE = np.array([0.0, 1.0])
-
-  # bell states in binary basis
-  _PHI_PLUS  = 1.0/np.sqrt(2.0) * (np.kron(_ZERO, _ZERO) + np.kron(_ONE, _ONE ))
-  _PHI_MINUS = 1.0/np.sqrt(2.0) * (np.kron(_ZERO, _ZERO) - np.kron(_ONE, _ONE ))
-  _PSI_PLUS  = 1.0/np.sqrt(2.0) * (np.kron(_ZERO, _ONE ) + np.kron(_ONE, _ZERO))
-  _PSI_MINUS = 1.0/np.sqrt(2.0) * (np.kron(_ZERO, _ONE ) - np.kron(_ONE, _ZERO))
-
-  # rotation matrices
-  _BIN_TO_BELL = np.stack([_PHI_PLUS, _PHI_MINUS, _PSI_PLUS, _PSI_MINUS])
-  _BELL_TO_BIN = _BIN_TO_BELL.T
-
-  # private constructor
-  def __init__(self, weights, basis)
-    """Constructor. 
+def _scale(state, factor):
+  for k in state:
+    state[k] *= factor
     
-    Really not intended for client use - they should the static factor methods instead.
+class QubitPair(dict):
 
-    Args:
-      weights (list): List of weights. Doesn't need to be normalized.
-      basis (str): Either "bell" or "binary": the basis to use, and basis of weights
+  def __repr__(self):
+    return "QubitPair({})".format(str(dict(self)))
 
-    Notes:
-      assumes sum of weights is not zero
-    """
-
-    self.vector = np.array(weights)
-    self.vector /= np.sum(self.vector)
-    self.basis = basis
-
-  # factory functions
   @classmethod
-  def singlet(cls, basis="bell"):
-    """Factory function for a singlet Bell state in a specific basis.
-    """
+  def singlet(cls, fidelity = 1.0):
+    state = cls({key: random.random() for key in ["phi+", "phi-", "psi+"]})
+    _normalize(state)
+    _scale(state, 1.0 - fidelity)
+    state["psi-"] = fidelity
+    return state
 
-    if basis == "binary":
-      weights = cls._PSI_MINUS
-    elif basis == "bell":
-      weights = np.array([0, 0, 0, 1.0])
-    else:
-      raise ValueError("Invalid basis: must be 'bell' or 'binary'")
-    return cls(weights, basis)
+  @classmethod
+  def pure_triplet(cls):
+    return {"phi+": 1.0, "phi-": 0.0, "psi+": 0.0, "psi-": 0.0}
 
-  # methods
-  def add_noise(self, stddev):
-    """Add Gaussian noise to a Qubit pair, with a given stddev.
-
-    Note that the internal state vector will be renormalized.
-    """
-
-    self.vector += stddev * np.random.randn(*self.vector.shape)
-    self.vector /= np.sum(self.vector)
-
-  def to_bell(self):
-    if self.basis == "bell":
-      return
-    if self.basis == "binary":
-      self.vector = self._BIN_TO_BELL.dot(self.vector)
-      self.basis = "bell"
   
-  def to_binary(self):
-    if self.basis == "binary":
-      return
-    if self.basis == "bell":
-      self.vector = self._BELL_TO_BIN.dot(self.vector)
-      self.basis = "binary"
-  
-  def fidelity(self):
-    """Get the fidelity (w.r.t. singlet state) of qubit pair.
-    """
-    # project
-    if self.basis == "bell":
-      out = self.vector[3] 
-    elif self.basis == "binary":
-      out = self._PSI_MINUS.dot(self.vector)
-
-    # convert amplitude to probability and return
-    out *= np.conj(out)
-    return out
 
 
---
+++ test_qubit_pair.py
@@ -0,0 +1,10 @@
+from qubit_pair import *
+
+def simple_test():
+  state = QubitPair.singlet()
+  print(state)
+
+if __name__ == "__main__":
+  print("Simple test:")
+  simple_test()
+  print()
